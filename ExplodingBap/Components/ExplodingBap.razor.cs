c3d06abdac71ad58ae4e690dd9b07e210a2ab8c1
Nick Gelotte <nick.gelotte@rainforgrowth.com>
2023-02-16 22:57:51 -0800

    V2 is looking pretty good. But need to hook up the button presses

---
+++ ExplodingBap/Components/ExplodingBap.razor.cs
@@ -0,0 +1,100 @@
+﻿using BAP.Types;
+using MessagePipe;
+using Microsoft.AspNetCore.Components;
+
+namespace ExplodingBap.Components
+{
+    [GamePage("Exploding Bap", "Can you hit the matches before they explode", UniqueId = "c4843f82-00cc-47df-8766-ce34744ac981")]
+    public partial class ExplodingBap : IGamePage, IDisposable
+    {
+        private string LastMessage = "";
+        private bool showLogs { get; set; } = false;
+        [Inject]
+        IGameProvider GameHandler { get; set; } = default!;
+        [Inject]
+        ISubscriber<GameEventMessage> GameEventPipe { get; set; } = default!;
+        IDisposable Subscriptions { get; set; } = default!;
+        protected override void OnInitialized()
+        {
+            var bag = DisposableBag.CreateBuilder();
+            GameEventPipe.Subscribe(async (x) => await GameUpdate(x)).AddTo(bag);
+            Subscriptions = bag.Build();
+            base.OnInitialized();
+        }
+
+        async Task GameUpdate(GameEventMessage e)
+        {
+            LastMessage = e.Message;
+            await InvokeAsync(() =>
+            {
+                StateHasChanged();
+            });
+        }
+
+
+        async Task<bool> ToggleLogs()
+        {
+            showLogs = !showLogs;
+            await InvokeAsync(() =>
+            {
+                StateHasChanged();
+            });
+            return true;
+        }
+
+        async Task<bool> StartGame()
+        {
+            if (GameHandler.CurrentGame == null)
+            {
+                GameHandler.UpdateToNewGameType(typeof(ExplodingBapGameV2));
+            }
+            if (GameHandler.CurrentGame != null)
+            {
+                await GameHandler.CurrentGame.Start();
+                await InvokeAsync(() =>
+                {
+                    StateHasChanged();
+                });
+                return true;
+            }
+            return false;
+        }
+
+        async Task<bool> EndGame()
+        {
+            if (GameHandler.CurrentGame != null)
+            {
+                await GameHandler.CurrentGame.ForceEndGame();
+                await InvokeAsync(() =>
+                {
+                    StateHasChanged();
+                });
+                return true;
+            }
+            return false;
+        }
+
+        public void Dispose()
+        {
+            if (Subscriptions != null)
+            {
+                Subscriptions.Dispose();
+            }
+        }
+
+        public Task<bool> NodesChangedAsync(NodeChangeMessage nodeChangeMessage)
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> LayoutChangedAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> GameUpdateAsync(GameEventMessage gameEventMessage)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ WebTest/App.razor.cs
@@ -0,0 +1,14 @@
+﻿using BAP.WebCore;
+using Microsoft.AspNetCore.Components;
+using System.Reflection;
+
+namespace WebTest
+{
+    public partial class App
+    {
+        [Inject]
+        LoadedAddonHolder loadedAddons { get; set; } = default!;
+
+
+    }
+}
